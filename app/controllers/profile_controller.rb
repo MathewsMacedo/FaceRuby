542f844d47a889fb02c21a1225b15ef2ba6572a3
Matheus Macedo <matheus.smacedo@hotmail.com>
2019-10-24 15:54:05 -0300

    Alterar Detalhes e Envio para o BD

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -9,7 +9,19 @@ class ProfileController < ApplicationController
 
     def biografia_post
         usuario = findUser
-        if usuario.update biografia_params
+        ver = usuario.update biografia_params
+        verificar ver
+    end
+
+    def detalhes_post 
+        usuario = findUser
+        ver = usuario.update detalhes_params
+        verificar ver
+    end
+
+
+    def verificar(usuario)
+        if usuario
             respond_to do |format|
                 format.json {render :json => usuario}
             end
@@ -18,18 +30,25 @@ class ProfileController < ApplicationController
         respond_to do |format|
             format.json{ render :status => "401", :json => {:message => "Unauthorized"}.to_json }
         end
-    end
-
+    end    
 
 
     private
+
+    def usuario_params
+        params.require(:usuario).permit(:id,:email,:senha)
+    end
 
     def biografia_params
         params.require(:usuario).permit(:id,:email,:senha,:biografia)
     end
 
+    def detalhes_params
+        params.require(:usuario).permit(:id,:email,:senha,:cidade_atual,:cidade_natal,:estado_civil)
+    end
+
     def findUser
-        user = Usuario.new biografia_params
+        user = Usuario.new usuario_params
         usuario = Usuario.where("id = ? and email = ? and senha = ?",user.id, user.email, user.senha) 
     end
 
--- public/js/profile.js
+++ public/js/profile.js
@@ -1,5 +1,16 @@
 var img_base64;
 var user;
+
+fetch('/userdata/'+localStorage.getItem('id'))
+.then(res => res.json())
+.then(function(json){
+    var usuario = JSON.parse(JSON.stringify(json));
+    user =  usuario;
+    loadProfile(usuario);
+});
+
+
+
 function getIMGJSON(send){
     const email = localStorage.getItem('email');
     const senha = localStorage.getItem('senha');
@@ -19,6 +30,20 @@ function getBiografiaJSON(){
     const biografia = document.querySelector('#biografia').value;
     const json = {usuario:{id: id,email: email,senha: senha,biografia: biografia}}
     return json;
+}
+
+
+function getDetalhesJSON(){
+
+    const email = localStorage.getItem('email');
+    const senha = localStorage.getItem('senha');
+    const id = localStorage.getItem('id');
+    const cidadeAtual = document.querySelector('input#cidade-atual').value;
+    const cidadeNatal = document.querySelector('input#cidade-natal').value;
+    const estadoCivil = document.querySelector('input#estado-civil').value;
+    const json = {usuario:{id: id,email: email,senha: senha,cidade_atual: cidadeAtual,cidade_natal: cidadeNatal,estado_civil: estadoCivil}}
+    return json;
+
 }
 
 
@@ -60,11 +85,10 @@ function sendPostagem(){
 
 function fixedBiografia() {
     var new_bio = document.querySelector('textarea#biografia').value;
-    document.querySelector('.biografia').innerHTML = "<span id=\"biografia\">" +
-     new_bio + "</span> <div class= \"btn-apresentacao\"><input type=\"button\" id=\"btn-biografia\" class=\"btn btn-default  btn-app\" value=\"Editar Biografia\"/></div>";
+    document.querySelector('.biografia').innerHTML = "<span id=\"biografia\"></span> <div class= \"btn-apresentacao\"><input type=\"button\" id=\"btn-biografia\" class=\"btn btn-default  btn-app\" value=\"Editar Biografia\"/></div>";
+    document.querySelector('span#biografia').textContent = new_bio;
     biografia();
   }
-
 
 
 function updateBiografia(){
@@ -77,22 +101,78 @@ function updateBiografia(){
         document.querySelector('#btn-biografia-update').addEventListener('click',function(){ sendBiografia(); fixedBiografia();});        
     }
 }
-
     
-    fetch('/userdata/'+localStorage.getItem('id'))
-    .then(res => res.json())
-    .then(function(json){
-        var usuario = JSON.parse(JSON.stringify(json));
-        user =  usuario;
-        loadProfile(usuario);
-    });
+function sendBiografia(){
+        var url = '/biografia_post'
+        var json = getBiografiaJSON();  
+        
+        set_POST(url,json);
+    
+    }
 
+
+function sendDetalhes(){
+    const url = '/detalhes_post'
+    const json = getDetalhesJSON();
+
+    set_POST(url,json);
+}
+
+
+function fixedDetalhes(){
+    var new_cidadeAtual = document.querySelector('input#cidade-atual').value;
+    var new_cidadeNatal = document.querySelector('input#cidade-natal').value;
+    var new_EstadoCivil = document.querySelector('input#estado-civil').value;
+
+    document.querySelector('.cidade-atual').innerHTML = `<div class="cidade-img"></div><span id="cidade-atual">Mora em <a id="cidade-atual" href=" " target="_blank"></a></span>`
+    document.querySelector('.cidade-natal').innerHTML = `<div class="cidade-natal-img"></div><span id="cidade-natal">De <a id="cidade-natal" href=" " target="_blank"></a></span>`
+    document.querySelector('.estado-civil').innerHTML = `<div class="estado-civil-img"></div><span id="estado-civil"></span>`  
+
+    document.querySelector('a#cidade-atual').href = "https://www.google.com/maps/place/"+new_cidadeAtual;
+    document.querySelector('a#cidade-natal').href = "https://www.google.com/maps/place/"+new_cidadeNatal;
+
+    document.querySelector('a#cidade-atual').textContent = new_cidadeAtual;
+    document.querySelector('a#cidade-natal').textContent = new_cidadeNatal;
+
+    document.querySelector('span#estado-civil').textContent = new_EstadoCivil;
+    document.querySelector('.btn-detalhes').innerHTML = `<input type="button" id="btn-detalhes" class="btn btn-default btn-app" value="Editar Detalhes">`
+    detalhes();
+}
+
+
+function updateDetalhes(){
+
+    if(document.querySelector('span#cidade-atual')){
+        var cidadeAtual = document.querySelector('a#cidade-atual').textContent;
+        var cidadeNatal = document.querySelector('a#cidade-natal').textContent;
+        var EstadoCivil = document.querySelector('span#estado-civil').textContent;
+
+        document.querySelector('.cidade-atual').innerHTML = `<div class="cidade-img"></div><input type="text" id="cidade-atual" name="cidade-atual" placeholder="Cidade Atual" class="form-group"/>`
+        document.querySelector('.cidade-natal').innerHTML = `<div class="cidade-natal-img"></div><input type="text" id="cidade-natal" name="cidade-natal" placeholder="Cidade Natal" class="form-group"/>`
+        document.querySelector('.estado-civil').innerHTML = `<div class="estado-civil-img"></div><input type="text" id="estado-civil" name="estado-civil" placeholder="Estado Civil"class="form-group"/>`
+        document.querySelector('.btn-detalhes').innerHTML = `<input type="button" id="btn-detalhes-update" class="btn btn-success  btn-app" value="Confirmar">`
+        document.querySelector('input#cidade-atual').focus();
+        document.querySelector('input#cidade-atual').value = cidadeAtual;
+        document.querySelector('input#cidade-natal').value = cidadeNatal;
+        document.querySelector('input#estado-civil').value = EstadoCivil;
+        document.querySelector('#btn-detalhes-update').addEventListener('click',function(){ sendDetalhes(); fixedDetalhes();});  
+    }
+
+
+}
 
 
 
   function sendIMG(send){
     var url = '/upload_img'
     var json = getIMGJSON(send);  
+
+    set_POST(url,json);
+
+}
+
+function set_POST(url,json){
+
     console.log(json);  
     fetch(url, {
         method: 'POST',
@@ -113,34 +193,6 @@ function updateBiografia(){
 
 }
 
-
-    function sendBiografia(){
-        var url = '/biografia_post'
-        var json = getBiografiaJSON();  
-        console.log(json);  
-        fetch(url, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(json)
-          }).then(function (response) {
-            if(response.status == 200){
-              alert('Success');
-            }else if(response.status == 401){
-              alert('Falha na alteração');
-            }else{
-              
-               // window.location.href= '/500';
-            }
-        });
-    
-    }
-
-
-
-
-
 function logoff(){
 
     localStorage.clear();
@@ -156,6 +208,9 @@ function biografia(){
     document.querySelector('#btn-biografia').addEventListener('click', function(){ updateBiografia() });
 }
 
+function detalhes(){
+    document.querySelector('#btn-detalhes').addEventListener('click', function(){ updateDetalhes() });
+}
 
 function sair(){
 
@@ -173,9 +228,17 @@ function uploadProfile(){
 
 
 function loadProfile(user){
-    document.querySelector('#capa-img').style.backgroundImage = "url(" + user.img_capa.toString() + ")"; 
-    document.querySelector('#profile-img').style.backgroundImage = "url(" + user.img_profile.toString() + ")"; 
-    document.querySelector('#biografia').textContent = user.biografia.toString();
+    document.querySelector('#capa-img').style.backgroundImage = "url(" + user.img_capa + ")"; 
+    document.querySelector('#profile-img').style.backgroundImage = "url(" + user.img_profile + ")"; 
+    document.querySelector('#biografia').textContent = user.biografia;
+    document.querySelector('a#cidade-atual').href = "https://www.google.com/maps/place/"+user.cidade_atual;
+    document.querySelector('a#cidade-natal').href = "https://www.google.com/maps/place/"+user.cidade_natal;
+
+    document.querySelector('a#cidade-atual').textContent = user.cidade_atual;
+    document.querySelector('a#cidade-natal').textContent = user.cidade_natal;
+
+    document.querySelector('span#estado-civil').textContent = user.estado_civil;
+    
     myElement = document.querySelectorAll('.post-img');
     for(let i = 0; i < myElement.length; i++){
         myElement[i].style.backgroundImage = "url(" + user.img_profile.toString() + ")"; 
@@ -190,5 +253,6 @@ window.addEventListener('load', function() {
     uploadIMG('#file-profile','#profile-img');
     publicar();
     biografia();
+    detalhes();
     sair();
   });
