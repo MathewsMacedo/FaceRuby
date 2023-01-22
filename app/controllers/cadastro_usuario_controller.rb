6d97abcc27737adb676c320a156bcfea2e6649db
Matheus Macedo <mmacedo@icasei.com.br>
2023-01-22 06:02:33 -0300

    fix: redirec

--- app/controllers/cadastro_usuario_controller.rb
+++ app/controllers/cadastro_usuario_controller.rb
@@ -19,12 +19,12 @@ class CadastroUsuarioController < ApplicationController
             puts e.message
             render json: { message: "Ocorreu um erro, tente novamente mais tarde!"}, status: 500
         end
-        
+
         if response.status > 400
             render json: { message: "Dados inválido, revise os dados e tente novamente"}, status: 400
         end
 
-        render json: {}, status: 201
+        redirect_to '/login'
     end
 
     private
